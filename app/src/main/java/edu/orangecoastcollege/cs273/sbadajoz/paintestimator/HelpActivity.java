947928edd4fbaf6058cfb3e507d22f6876f0c2ef
Severiano Badajoz <sevebadajoz@gmail.com>
2017-09-25 20:22:20 -0700

    Complete functionality

--- app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/paintestimator/HelpActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/paintestimator/HelpActivity.java
@@ -1,7 +1,10 @@
 package edu.orangecoastcollege.cs273.sbadajoz.paintestimator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        Intent intent = getIntent();
+        ((TextView)findViewById(R.id.amountTextView)).setText("Estimated Paint Required: " + intent.getFloatExtra("gallons" , 0) + " gallons");
+    }
+
+    protected void goBack(View view) {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
